6c4730ed3df922963e8ec171b3b6310c12ca1b59
aashabul <aashabulimam@gmail.com>
2022-05-31 20:23:54 +0600

    homescreen updated

--- src/screens/home.js
+++ src/screens/home.js
@@ -11,21 +11,28 @@ import Text from "../components/text/text";
 import PlanetHeader from "../components/planet-header";
 import { colors } from "../theme/colors";
 import { PLANET_LIST } from "../data/planet-list";
+import { spacing } from "../theme/spacing";
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader />
       <FlatList
+        contentContainerStyle={styles.list}
         data={PLANET_LIST}
         keyExtractor={(item) => item.name}
         renderItem={({ item }) => {
+          const { name, color } = item;
           return (
-            <View>
-              <Text>{item.name}</Text>
+            <View style={styles.item}>
+              <View style={[styles.circle, { backgroundColor: color }]} />
+              <Text preset="h4" style={styles.itemName}>
+                {name}
+              </Text>
             </View>
           );
         }}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
       />
     </SafeAreaView>
   );
@@ -35,5 +42,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: colors.black,
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  list: {
+    padding: spacing[5],
+  },
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: spacing[4],
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  itemName: {
+    textTransform: "uppercase",
+    marginLeft: spacing[4],
+  },
+  separator: {
+    borderBottomColor: colors.white,
+    borderWidth: 0.5,
   },
 });
