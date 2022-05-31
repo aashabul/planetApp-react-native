10d91aff321704b308a6ad5e7200e7a92969ce10
aashabul <aashabulimam@gmail.com>
2022-05-31 19:49:44 +0600

    data showing, flatlist working

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, StatusBar } from "react-native";
 import { colors } from "./src/theme/colors";
 import { spacing } from "./src/theme/spacing";
 import { useFonts } from "expo-font";
 import { typography } from "./src/theme/typography";
 import Text from "./src/components/text/text";
-import { DarkTheme, NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./src/screens/home";
 
@@ -22,11 +21,12 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <NavigationContainer theme={DarkTheme}>
+      <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
+      <StatusBar style="light" />
     </View>
   );
 }
--- src/screens/home.js
+++ src/screens/home.js
@@ -4,20 +4,36 @@ import {
   StyleSheet,
   Platform,
   StatusBar,
+  FlatList,
 } from "react-native";
 import React from "react";
 import Text from "../components/text/text";
-
-const androidView = StyleSheet.create({
-  AndroidSafeArea: {
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
+import PlanetHeader from "../components/planet-header";
+import { colors } from "../theme/colors";
+import { PLANET_LIST } from "../data/planet-list";
 
 export default function Home() {
   return (
-    <SafeAreaView style={androidView.AndroidSafeArea}>
-      <Text>Home</Text>
+    <SafeAreaView style={styles.container}>
+      <PlanetHeader />
+      <FlatList
+        data={PLANET_LIST}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text>{item.name}</Text>
+            </View>
+          );
+        }}
+      />
     </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.black,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+});
