a1303cf634bcb980cc8748ded71e5d733e2a8c06
aashabul <aashabulimam@gmail.com>
2022-05-31 16:09:08 +0600

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { colors } from "./src/theme/colors";
+import { spacing } from "./src/theme/spacing";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Antonio-Medium": require("./assets/fonts/Antonio-Medium.ttf"),
+    "Spartan-Bold": require("./assets/fonts/Spartan-Bold.ttf"),
+    "Spartan-Regular": require("./assets/fonts/Spartan-Regular.ttf"),
+  });
+  if (!loaded) {
+    return <Text>Font is loading...</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: "Antonio-Medium" }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text style={{ marginTop: spacing[4], fontFamily: "Spartan-Bold" }}>
+        Hello World
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.darkOrange,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
