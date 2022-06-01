2e0f3be76c17e72dbda02131a4e81c127086eec5
aashabul <aashabulimam@gmail.com>
2022-06-01 14:52:52 +0600

    details completed

--- src/screens/details.js
+++ src/screens/details.js
@@ -4,6 +4,8 @@ import {
   StatusBar,
   StyleSheet,
   ScrollView,
+  Pressable,
+  Linking,
 } from "react-native";
 import React from "react";
 import Text from "../components/text/text";
@@ -21,12 +23,31 @@ import {
   NeptuneSvg,
 } from "../svg";
 
-export default function Details({ navigation, route }) {
-  const planet = route.params.planet;
-  const { name } = planet;
-  console.log("PLANET-->", planet);
+const PlanetSection = ({ title, value }) => {
+  return (
+    <View style={styles.planetSection}>
+      <Text preset="small" style={{ textTransform: "uppercase" }}>
+        {title}
+      </Text>
+      <Text preset="h2">{value}</Text>
+    </View>
+  );
+};
 
-  const renderImage = (name) => {
+export default function Details({ route }) {
+  const planet = route.params.planet;
+  const {
+    name,
+    description,
+    rotationTime,
+    revolutionTime,
+    radius,
+    avgTemp,
+    wikiLink,
+  } = planet;
+  // console.log("PLANET-->", planet);
+
+  const renderImage = () => {
     switch (name) {
       case "mercury":
         return <MercurySvg />;
@@ -47,11 +68,32 @@ export default function Details({ navigation, route }) {
     }
   };
 
+  const onPressLink = () => {
+    Linking.openURL(wikiLink);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader backBtn={true} />
       <ScrollView>
-        <View style={styles.imageView}>{renderImage(name)}</View>
+        <View style={styles.imageView}>{renderImage()}</View>
+        <View style={styles.detailsView}>
+          <Text preset="h1" style={styles.name}>
+            {name}
+          </Text>
+          <Text style={styles.description}>{description}</Text>
+          <Pressable onPress={onPressLink} style={styles.source}>
+            <Text>Source: </Text>
+            <Text preset="h4" style={styles.wikipedia}>
+              Wikipedia
+            </Text>
+          </Pressable>
+        </View>
+        <View style={{ height: 40 }} />
+        <PlanetSection title="ROTATION TIME" value={rotationTime} />
+        <PlanetSection title="REVOLUTION TIME" value={revolutionTime} />
+        <PlanetSection title="RADIUS" value={radius} />
+        <PlanetSection title="AVERAGE TEMP." value={avgTemp} />
       </ScrollView>
     </SafeAreaView>
   );
@@ -64,8 +106,42 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   imageView: {
-    padding: spacing[5],
+    padding: spacing[8],
     alignItems: "center",
     justifyContent: "center",
+  },
+  detailsView: {
+    // marginTop: spacing[10],
+    marginHorizontal: spacing[6],
+    alignItems: "center",
+  },
+  name: {
+    textTransform: "uppercase",
+    textAlign: "center",
+  },
+  description: {
+    textAlign: "center",
+    marginTop: spacing[5],
+    lineHeight: 21,
+  },
+  source: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: spacing[5],
+  },
+  wikipedia: {
+    textDecorationLine: "underline",
+    fontWeight: "bold",
+  },
+  planetSection: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: spacing[5],
+    paddingVertical: spacing[4],
+    borderWidth: 1,
+    borderColor: colors.grey,
+    marginHorizontal: spacing[6],
+    marginBottom: spacing[4],
   },
 });
