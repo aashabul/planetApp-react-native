9104fda1d4a883b3249a7e4357be2ff82899e196
aashabul <aashabulimam@gmail.com>
2022-06-01 15:21:21 +0600

    search engine working

--- src/screens/home.js
+++ src/screens/home.js
@@ -6,8 +6,9 @@ import {
   StatusBar,
   FlatList,
   Pressable,
+  TextInput,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Text from "../components/text/text";
 import PlanetHeader from "../components/planet-header";
 import { colors } from "../theme/colors";
@@ -38,15 +39,36 @@ const PlanetItem = ({ item }) => {
 };
 
 export default function Home({ navigation }) {
+  const [list, setList] = useState(PLANET_LIST);
   const renderItem = ({ item }) => {
     return <PlanetItem item={item} />;
   };
+
+  const searchFilter = (text) => {
+    const filteredList = PLANET_LIST.filter((item) => {
+      const itemName = item.name.toLowerCase();
+      const userTypedText = text.toLowerCase();
+      return itemName.indexOf(userTypedText) > -1;
+    });
+    setList(filteredList);
+    // console.log(filteredList);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader />
+      <TextInput
+        placeholder="Type the planet name"
+        placeholderTextColor={colors.white}
+        autoCorrect={false}
+        style={styles.searchInput}
+        onChangeText={(text) => {
+          searchFilter(text);
+        }}
+      />
       <FlatList
         contentContainerStyle={styles.list}
-        data={PLANET_LIST}
+        data={list}
         keyExtractor={(item) => item.name}
         renderItem={renderItem}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
@@ -81,5 +103,12 @@ const styles = StyleSheet.create({
   separator: {
     borderBottomColor: colors.white,
     borderWidth: 0.5,
+  },
+  searchInput: {
+    padding: spacing[4],
+    color: colors.white,
+    borderBottomColor: colors.white,
+    borderBottomWidth: 1,
+    margin: spacing[5],
   },
 });
